75509d217ab6cbf84729ccb1074a05ce68cc5859
Ben Harris <mail@benharris.ch>
2016-09-02 10:37:15 -0400

    java works up to 10000 relatively quickly. no threads yet

--- java_src/Collatz.java
+++ java_src/Collatz.java
@@ -1,13 +1,36 @@
 class Collatz {
-  public static void main() {
-    System.out.println("collatz");
+
+  int prev[] = new int[1000000];
+  int max = 0;
+  int maxsteps = 0;
+
+  public static void main(String[] args) {
+    System.out.println("collatz calc");
+
+    Collatz c = new Collatz();
+
+    for(int i = 1; i <= 100000; i++){
+      System.out.println(i + "\tnum of steps: " + c.collatz(i));
+    }
+
+    System.out.println("Longest path was at " + c.max + " with " + c.maxsteps + " steps.");
   }
 
   int collatz(int start) {
     int cnt = 0;
+    int initval = start;
     while(start != 1) {
       cnt++;
+      if(start < 1000000 && prev[start] != 0) {
+        start = prev[start];
+        continue;
+      }
       start = start % 2 == 0 ? start/2 : 3*start + 1;
+    }
+    prev[start] = cnt;
+    if(cnt > max){
+      max = initval;
+      maxsteps = cnt;
     }
     return cnt;
   }
